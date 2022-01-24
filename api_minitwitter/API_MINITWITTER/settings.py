38e2395f954da17894c328af13e2a0d8259cf03c
Julia <juliaguardiani@icloud.com>
2022-01-24 19:08:08 -0300

    AmazonS3

--- api_minitwitter/API_MINITWITTER/settings.py
+++ api_minitwitter/API_MINITWITTER/settings.py
@@ -126,6 +126,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
 
+AWS_ACCESS_KEY_ID = 'AKIAWREX5PLF6YBDBU7U'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'julia-minitwitter'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
