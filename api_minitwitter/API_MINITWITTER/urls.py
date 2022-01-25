6e9800e18ab3810c573793cc214db60005343566
Julia <juliaguardiani@icloud.com>
2022-01-24 21:26:55 -0300

    autenticacao jwt

--- api_minitwitter/API_MINITWITTER/urls.py
+++ api_minitwitter/API_MINITWITTER/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 from tweets.api import viewsets as tweetsviewsets
 
@@ -25,5 +26,7 @@ route.register(r'tweets', tweetsviewsets.TweetsViewSet, basename="Tweets")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('', include(route.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api_minitwitter/tweets/api/viewsets.py
+++ api_minitwitter/tweets/api/viewsets.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
-#from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 from tweets.api import serializers
 from tweets import models
 
 
 class TweetsViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     serializer_class = serializers.TweetsSerializer
     queryset = models.Tweets.objects.all() #Todos os obj dos modelos.
